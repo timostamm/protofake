fe5f59ca724abaa75e2847541697d8135e6d8c55
Timo Stamm <ts@timostamm.de>
2024-08-19 21:51:16 +0200

    Fix lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,6 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config({
-  files: ["src/**/*.ts"],
-  ignores: ["src/gen/**/*"],
+  ignores: ["src/gen/**/*", "dist"],
   extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
 });
